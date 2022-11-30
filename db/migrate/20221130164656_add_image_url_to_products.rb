2e8e8da511790f3c1c2228f9c0561159593e3196
Daniel Pearson <pearsondg@gmail.com>
2022-11-30 17:25:17 +0000

    small changes to home pages and routes, added a scrape to seed file

---
+++ db/migrate/20221130164656_add_image_url_to_products.rb
@@ -0,0 +1,5 @@
+class AddImageUrlToProducts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :products, :image_url, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+require "nokogiri"
+
+# url = "https://www.trolley.co.uk/browse/household?page=1"
+
+
+i = 1
+
+until i == 10
+  url = "https://www.trolley.co.uk/browse/household?page=#{i}"
+
+  html_file = URI.open(url).read
+html_doc = Nokogiri::HTML(html_file)
+
+html_doc.search(".product-item").each do |element|
+  brand = element.css('._brand').text.strip
+  item = element.css('._desc').text.strip
+  image_code = element.attribute('data-id').value
+  product = Product.new(
+    item: item,
+    brand: brand,
+    image_url: "https://www.trolley.co.uk/img/product/#{image_code}"
+    )
+  product.save
+end
+puts "created #{Product.count} products"
+i += 1
+end
