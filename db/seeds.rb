8fe8eea328a5bd2d4829c41a345aaf2af2f33bc8
Emilio Marcos <emilio7marcos@gmail.com>
2022-11-30 12:06:37 +0000

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+List.destroy_all
+User.destroy_all
+
+puts "creating new islands..."
+
+User.create!(email: "john@email.com", password: "password")
+List.create!(name: "Home", user: User.first)
+User.create!(email: "michael@email.com", password: "password")
+List.create!(name: "Home", user: User.second)
+
+puts "created #{User.count} users"
+puts "created #{List.count} lists"
