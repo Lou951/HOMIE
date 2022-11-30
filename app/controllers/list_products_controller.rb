589255f64e719f1c840347d96bb54f36add2b299
Andrew Birch <birch.fish@gmail.com>
2022-11-30 15:24:58 +0000

    fixing list products

--- app/controllers/list_products_controller.rb
+++ app/controllers/list_products_controller.rb
@@ -1,7 +1,7 @@
 class ListProductsController < ApplicationController
   before_action :set_list, only: %i[new create]
   before_action :set_user, only: %i[new create]
-  before_action :set_product, only: %i[new create]
+  before_action :set_product, only: %i[create]
 
   def new
     @list_product = ListProduct.new
@@ -18,24 +18,31 @@ class ListProductsController < ApplicationController
 
   def create
     @list_product = ListProduct.new(list_product_params)
-    @list_product.save
+    @list_product.product = @product
+    @list_product.user = @user
+    @list_product.list = @list
+    if @list_product.save
+      redirect_to list_path(@list), notice: 'Your list was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_product
-    @list_product.product = Product.find(params[:product_id])
+    @product = Product.find(params[:list_product][:product_id])
   end
 
   def set_list
-    @list_product.list = List.find(params[:list_id])
+    @list = List.find(params[:list_id])
   end
 
   def set_user
-    @list_product.user = current_user
+    @user = current_user
   end
 
   def list_product_params
-    params.require(:list_product).permit(:frequency, :list_id, :product_id)
+    params.require(:list_product).permit(:frequency, :list_id, :product_id, :user_id)
   end
 end
