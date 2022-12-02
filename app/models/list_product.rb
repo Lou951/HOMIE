6356842ae708774089ac5a947dd955fd6f3d2731
Louis Cox <louiscox951@gmail.com>
2022-12-02 11:53:06 +0000

    added dependent destroy

--- app/models/list_product.rb
+++ app/models/list_product.rb
@@ -2,6 +2,6 @@ class ListProduct < ApplicationRecord
   belongs_to :user
   belongs_to :list
   belongs_to :product
-  has_many :purchases
+  has_many :purchases, dependent: :destroy
   validates :frequency, presence: true
 end
