3fa59c4dcead6a41020f5876983dba12c5c74329
Louis Cox <louiscox951@gmail.com>
2022-12-05 13:07:19 +0000

    cards change pics

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,18 @@ class List < ApplicationRecord
   has_many :list_products, dependent: :destroy
   has_many :users, through: :user_lists
   validates :name, presence: true
+
+
+  def self.card_pic(placeholder)
+    case placeholder.capitalize
+    when "Home"
+      "https://res.cloudinary.com/de4yia9u4/image/upload/v1670242444/z69cn8xetmwbandw4w3m.jpg"
+    when "Work"
+      "https://res.cloudinary.com/de4yia9u4/image/upload/v1670241974/kgiay14unp5lnaqjhmsg.jpg"
+    when "Personal"
+      "https://res.cloudinary.com/de4yia9u4/image/upload/v1670242329/xhstoanskcdd8cbzvf6g.jpg"
+    else
+      "https://res.cloudinary.com/de4yia9u4/image/upload/v1670242272/zmd5aud9kmf1e4rzwsrb.jpg"
+    end
+  end
 end
