fcab2d7d73a193c75bf1800b4390c9492349a79e
Andrew Birch <birch.fish@gmail.com>
2022-12-06 16:05:47 +0000

    wallet fix

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,7 +1,7 @@
 class WalletsController < ApplicationController
 
   def wallet
-    @purchases = Purchase.where(user_id: current_user)
+    @purchases = Purchase.where(user_id: current_user).where(created_at: (Date.today - 2.month))
   end
 
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -12,11 +12,12 @@ class List < ApplicationRecord
   end
 
   def list_products_total(list)
-  sum = 0
+    sum = 0
     list.list_products.each do |el|
-      sum += el.purchases.sum(&:price_paid)
+      # sum += el.purchases.where(created_at: (Time.now - 1.month)..Time.now).sum(&:price_paid)
+      el.purchases.each {|purchase| sum += purchase.price_paid}
     end
-  sum
+    sum
   end
 
   def self.card_pic(placeholder)
