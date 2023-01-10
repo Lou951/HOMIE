9ed4f1b5629cce68cfec94258f88033dae321d5e
Andrew Birch <andrewbirch1882@icloud.com>
2023-01-10 12:25:56 +0000

    Moved countdown logic to list product model

--- app/models/list_product.rb
+++ app/models/list_product.rb
@@ -4,4 +4,8 @@ class ListProduct < ApplicationRecord
   belongs_to :product
   has_many :purchases, dependent: :destroy
   validates :frequency, presence: true
+
+  def countdown
+    (self.next_purchase - Date.today).to_i
+  end
 end
